68216ec45a457ac8133b321be5a2e15b470a9653
Marcos Aurélio Macedo da Rocha Júnior <marcosau_2809@hotmail.com>
2020-08-28 08:16:12 -0300

    Renomeando classes para atender padrão camelCase

--- src/main/java/br/com/marcos/matriculasquery/MatriculasQueryApplication.java
+++ src/main/java/br/com/marcos/matriculasquery/MatriculasQueryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MatriculasqueryApplication {
+public class MatriculasQueryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MatriculasqueryApplication.class, args);
+		SpringApplication.run(MatriculasQueryApplication.class, args);
 	}
 
 }
