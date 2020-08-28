65b00d45b1f8b9088f0d79d0c39ab87effb7f3c7
Marcos Aurélio Macedo da Rocha Júnior <marcosau_2809@hotmail.com>
2020-08-28 08:09:10 -0300

    Commit inicial

---
+++ src/main/java/br/com/marcos/matriculasquery/MatriculasqueryApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.matriculasquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculasqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculasqueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcos/matriculasquery/MatriculasqueryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.matriculasquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatriculasqueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
